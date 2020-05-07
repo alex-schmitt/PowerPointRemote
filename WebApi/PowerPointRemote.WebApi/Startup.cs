830c0ec4d810bf9c965571cb763506ad5969b244
Alex Schmitt <39417513+alex-schmitt@users.noreply.github.com>
2020-05-06 21:41:39 -0500

    Use forwarded headers for Web API
    - Web API will be behind a reverse proxy

--- WebApi/PowerPointRemote.WebApi/Startup.cs
+++ WebApi/PowerPointRemote.WebApi/Startup.cs
@@ -83,10 +83,14 @@ namespace PowerPointRemote.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
-            app.UseHttpsRedirection();
             app.UseRouting();
 
             if (env.IsDevelopment())
