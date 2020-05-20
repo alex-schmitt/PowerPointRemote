c2ccee5be4f2747dde5fc77b8f324afeae8f067d
Alex Schmitt <39417513+alex-schmitt@users.noreply.github.com>
2020-05-19 22:24:32 -0500

    Add WPF template

---
+++ DesktopClient/PowerPointRemote.DesktopClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace PowerPointRemote.DesktopClient
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ DesktopClient/PowerPointRemote.DesktopClient/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PowerPointRemote.DesktopClient
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
