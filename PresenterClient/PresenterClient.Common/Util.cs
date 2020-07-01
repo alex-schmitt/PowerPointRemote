ecefc9e557ac6a66d482c25d48511275fd74c524
Alex Schmitt <39417513+alex-schmitt@users.noreply.github.com>
2020-07-01 17:54:55 -0500

    Add common project

---
+++ PresenterClient/PresenterClient.Common/Util.cs
@@ -0,0 +1,17 @@
+﻿namespace PresenterClient.Common
+{
+    public static class Util
+    {
+        public static bool IsDebug
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}
