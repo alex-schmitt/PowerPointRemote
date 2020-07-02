50e8a7529ff22a0a2f888f7311229f27c5aa34d5
Alex Schmitt <39417513+alex-schmitt@users.noreply.github.com>
2020-07-01 23:41:36 -0500

    Add SlideShow project with window tracker

---
+++ PresenterClient/PresenterClient.SlideShow/SlideShowWindowTracker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace PresenterClient.SlideShow
+{
+    public class SlideShowWindowTracker
+    {
+        private readonly Application _application;
+
+        public SlideShowWindow SlideShowWindow;
+
+        /// <summary>
+        ///     Tracks the last active SlideShowWindow for a given PowerPoint application.
+        /// </summary>
+        /// <param name="application"></param>
+        public SlideShowWindowTracker(Application application)
+        {
+            _application = application;
+
+            SlideShowWindow = application.SlideShowWindows.Cast<SlideShowWindow>().LastOrDefault();
+
+            application.SlideShowNextSlide += ApplicationOnSlideShowNextSlide;
+            application.SlideShowEnd += ApplicationOnSlideShowEnd;
+        }
+
+        public event EventHandler<SlideShowWindow> OnSlideShowWindowChange;
+
+        private void ApplicationOnSlideShowEnd(Presentation presentation)
+        {
+            /*
+                It's possible that the last active slide show was not the slide show that ended.
+                The first clause is true if the active slide show ended manually.
+                The second clause is true if the active slide show ended by clicking through to the end.
+             */
+            if (!_application.SlideShowWindows.Cast<SlideShowWindow>().Contains(SlideShowWindow) ||
+                SlideShowWindow.View.State == PpSlideShowState.ppSlideShowDone)
+                SlideShowWindow = null;
+        }
+
+        private void ApplicationOnSlideShowNextSlide(SlideShowWindow slideShowWindow)
+        {
+            if (SlideShowWindow != slideShowWindow)
+                OnSlideShowWindowChange?.Invoke(this, slideShowWindow);
+        }
+    }
+}
