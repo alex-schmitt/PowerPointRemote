251d14d3ed9a250e10d82d629de5461c92caf0aa
Alex Schmitt <39417513+alex-schmitt@users.noreply.github.com>
2020-10-25 00:57:43 -0500

    Remove old web-client

--- WebClient/src/api/channel.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { apiAddress } from "../constants";
-
-const webapi = axios.create({ baseURL: apiAddress });
-
-const joinChannel = (userName, channelId) => webapi.post("/join-channel", { userName, channelId });
-
-export const channelApi = { joinChannel };
--- WebClient/src/constants.js
+++
@@ -1,3 +0,0 @@
-const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-
-export const apiAddress = isDev ? "https://localhost:5001" : "https://api.ppremote.com";
